27202fa2b0ca1557897bc5631895a1b8f1dd8dc5
Alexandr Sobko <alexfreetibet@gmail.com>
2021-02-21 14:19:32 +0700

    Запрос и обработка ответа о погоде вынесены в отдельный класс, в отдельном файле

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import ephem
-import requests
 import settings
 from datetime import datetime
+from utils import WeatherInfoOpenWeatherMap
 
 
 def greet_user(update, context):
@@ -37,35 +37,34 @@ def planet(update, context):
 
 
 def weather(update, context):
-    city = 'Tomsk'
-    req = requests.get(
-        f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={settings.WEATHER_APIKEY}&lang=ru&units=metric'
-    )
-    if req.status_code == requests.codes.ok:
-        info = req.json()
-        city_name = info['name']
-        weather_info = info['weather']
-        icon_url = f"http://openweathermap.org/img/wn/{weather_info[0]['icon']}@2x.png"
-        descr = weather_info[0]['description']
-        temp = info['main']['temp']
-        temp_feel = info['main']['feels_like']
-        pressure = info['main']['pressure']
-        humidity = info['main']['humidity']
-        wind_speed = info['wind']['speed']
-        answer = f"*{city_name} - {descr}*\n`Температура:` {temp}°C  `По ощущениям` {temp_feel}°C\n" \
-                 f"`Давление: {pressure}, Влажность: {humidity}`\n`Скорость ветра {wind_speed}м/с`\n{icon_url}"
-        update.message.reply_text(answer, parse_mode="Markdown")
-    else:
-        update.message.reply_text("`Неудачная попытка связаться с openweathermap api`", parse_mode="Markdown")
+    try:
+        city = update.message.text.split()[1]
+    except IndexError:
+        city = settings.DEFAULT_CITY
+
+    info = WeatherInfoOpenWeatherMap(city)
+
+    if not info.search():
+        return update.message.reply_text('`Ошибка`\n`Нет данных`', parse_mode="Markdown")
+
+    answer = f"*{info.name} - {info.description}*\n`Температура:` {info.temp}°C  `По ощущениям` {info.temp_feel}°C\n" \
+             f"`Давление: {info.pressure}, Влажность: {info.humidity}`\n" \
+             f"`Скорость ветра {info.wind_speed}м/с`\n{info.icon_url}"
+
+    update.message.reply_text(answer, parse_mode="Markdown")
 
 
 def main():
-    logging.basicConfig(filename='bot.log', level=logging.INFO)
-    bot = Updater(settings.API_KEY, use_context=True)
+    logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s', filename='bot.log', level=logging.INFO)
+    bot = Updater(settings.API_KEY, request_kwargs=settings.PROXY, use_context=True)
     dp = bot.dispatcher
+
     dp.add_handler(CommandHandler('start', greet_user))
     dp.add_handler(CommandHandler('weather', weather))
+    dp.add_handler(CommandHandler('planet', planet))
+
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
     logging.info('Bot starting')
     bot.start_polling()
     bot.idle()
---
+++ utils.py
@@ -0,0 +1,37 @@
+import requests
+import settings
+
+
+class WeatherInfoOpenWeatherMap:
+    _api_key = settings.WEATHER_APIKEY
+
+    def __init__(self, name):
+        self._city_name = name
+
+    def _weather_info_separates(self, weather_info: dict):
+        self.name = weather_info['name']
+
+        main_info = weather_info['main']
+        self.temp = main_info['temp']
+        self.temp_feel = main_info['feels_like']
+        self.pressure = main_info['pressure']
+        self.humidity = main_info['humidity']
+
+        self.wind_speed = weather_info['wind']['speed']
+
+        description_and_icon = weather_info['weather'][0]
+        self.description = description_and_icon['description']
+        self.icon_url = f"http://openweathermap.org/img/wn/{description_and_icon['icon']}@2x.png"
+
+    def search(self) -> bool:
+        weather_info = requests.get(
+            f'http://api.openweathermap.org/data/2.5/weather?'
+            f'q={self._city_name}'
+            f'&appid={self._api_key}&lang=ru&units=metric'
+        )
+
+        if weather_info.status_code != requests.codes.ok:
+            return False
+
+        self._weather_info_separates(weather_info.json())
+        return True
