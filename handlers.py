546118974b59ad4f9d22bbe24d89081c66b0c0ee
Alexandr Sobko <alexfreetibet@gmail.com>
2021-03-02 00:29:48 +0700

    Переносы строк слешами заменены на строки в скобках

--- handlers.py
+++ handlers.py
@@ -12,7 +12,7 @@ def greet_user(update, context):
         f'{settings.DESCRIPTION}',
         parse_mode="Markdown",
         reply_markup=user_keyboard()
-        )
+    )
 
 
 def talk_to_me(update, context):
@@ -64,8 +64,10 @@ def planet(update, context):
     date = datetime.now().strftime("%Y/%m/%d")
     target = getattr(ephem, query)(date)
     target_position, target_position_full = ephem.constellation(target)
-    answer = f"`Планета` {target.name} `сегодня в созвездии` {target_position}\n" \
-             f"`Также известном как` {target_position_full}"
+    answer = (
+        f"`Планета` {target.name} `сегодня в созвездии` {target_position}\n"
+        f"`Также известном как` {target_position_full}"
+    )
 
     update.message.reply_text(answer, parse_mode="Markdown", reply_markup=user_keyboard())
 
@@ -81,53 +83,55 @@ def weather(update, context):
     if not info.search():
         return update.message.reply_text('`Ошибка`\n`Нет данных`', parse_mode="Markdown")
 
-    answer = f"*{info.name} - {info.description}*\n`Температура:` {info.temp}°C  `По ощущениям` {info.temp_feel}°C\n" \
-             f"`Давление: {info.pressure}, Влажность: {info.humidity}`\n" \
-             f"`Скорость ветра {info.wind_speed}м/с`\n{info.icon_url}"
-
+    answer = (
+        f"*{info.name} - {info.description}*\n`Температура:` {info.temp}°C  `По ощущениям` {info.temp_feel}°C\n"
+        f"`Давление: {info.pressure}, Влажность: {info.humidity}`\n"
+        f"`Скорость ветра {info.wind_speed}м/с`\n{info.icon_url}"
+    )
     update.message.reply_text(answer, parse_mode="Markdown", reply_markup=user_keyboard())
 
 
 def city_game(update, context):
-    if 'city_game' not in context.user_data:
+    game = context.user_data.get('city_game')
+
+    if game is None:
         game = CityGame()
         game.start_new_game()
-        context.user_data['city_game'] = game
 
     try:
-        city_name = update.message.text.split()[1]
+        player_city = update.message.text.split()[1]
     except IndexError:
-        return update.message.reply_text(
-            '`Введите после "Город" название города на русском языке.`'
-            '`Если название города состоит из нескольких слов то разделяйте их дефисами "-"`',
-            parse_mode="Markdown"
+        answer = (
+            '`Введите после "Город" название города на русском языке.`\n'
+            '`Если название города состоит из нескольких слов то разделяйте их дефисами "-"`'
         )
-
-    last_char = context.user_data['city_game'].last_char
-
-    if city_name[0].lower() != last_char and last_char is not None:
-        return update.message.reply_text(
-            f"{city_name} `начинается не на` *{last_char}*", parse_mode="Markdown"
-        )
-
-    if not context.user_data['city_game'].check_city(city_name.lower()):
-        return update.message.reply_text(
-            f"`Города {city_name} нет в моей базе данных, попробуйте ещё раз`", parse_mode="Markdown"
-        )
-
-    new_city = context.user_data['city_game'].get_city(city_name.lower()[-1])
-
-    if new_city is None:
-        game = context.user_data.pop('city_game')
-        answer = f"*Поздравляю* {update.effective_chat['username']}!\n" \
-                 f"`Городов начинающихся на` *{city_name[-1]}* `больше нет в моей базе данных.`\n" \
-                 f"`Вы выйграли назвав` *{game.score}* `города(ов).`"
         return update.message.reply_text(answer, parse_mode="Markdown")
 
-    update.message.reply_text(
-        f"`Ваш город:` {city_name} `заканчивается на букву` *{city_name[-1].upper()}*\n"
-        f"`Мой ответ:` {new_city.capitalize()}.\n"
-        f"`Теперь Вам город на букву` *{new_city[-1].upper()}*",
-        parse_mode="Markdown",
-        reply_markup=user_keyboard()
+    if not game.check_last_char(player_city):
+        return update.message.reply_text(f"{player_city} `начинается не на` *{game.last_char}*", parse_mode="Markdown")
+
+    if not game.check_city(player_city.lower()):
+        return update.message.reply_text(
+            f"`Города {player_city} нет в моей базе данных, попробуйте ещё раз`", parse_mode="Markdown"
+        )
+
+    new_city = game.get_city(player_city.lower())
+    if new_city is None:
+        username = update.effective_chat['username']
+        answer = (
+            f"*Поздравляю* {username}!\n"
+            f"`Городов начинающихся на` *{game.last_char}* `больше нет в моей базе данных.`\n"
+            f"`Вы выйграли назвав` *{game.score}* `города(ов).`"
+        )
+        context.user_data.pop('city_game')
+        return update.message.reply_text(answer, parse_mode="Markdown")
+
+    context.user_data['city_game'] = game
+    last_char = player_city[-1].upper()
+    new_last_char = game.last_char
+    answer = (
+        f"`Ваш город:` {player_city} `заканчивается на букву` *{last_char}*\n"
+        f"`Мой ответ:` {new_city.capitalize()}.\n`Теперь Вам город на букву` *{new_last_char}*\n"
+        f"На данный момент ваш счёт равен` *{game.score}*"
     )
+    update.message.reply_text(answer, parse_mode="Markdown", reply_markup=user_keyboard())
